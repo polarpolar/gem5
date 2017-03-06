6641a49922af1ca0552e4d98fb2504a121afb7ea
zlfben <zlfben17@gmail.com>
2017-03-06 04:17:38 +0000

    m5: add compatibility for non-energy simulations

--- src/python/m5/simulate.py
+++ src/python/m5/simulate.py
@@ -111,7 +111,8 @@ def instantiate(ckpt_dir=None):
     # Create the C++ sim objects and connect ports
     for obj in root.descendants(): obj.createCCObject()
     for obj in root.descendants(): obj.connectPorts()
-    root.system.connect_energy_ports()
+    if hasattr(root.system, 'energy_mgmt'):
+        root.system.connect_energy_ports()
 
     # Do a second pass to finish initializing the sim objects
     for obj in root.descendants(): obj.init()
